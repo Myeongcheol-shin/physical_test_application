34e495da0606d034d4fc2a558388ef1180dd305f
shino72 <a36243845@gmail.com>
2023-05-31 22:13:45 +0900

    리더보드 기능 추가

--- app/src/main/java/com/shino72/physical/ui/main/MainActivity.kt
+++ app/src/main/java/com/shino72/physical/ui/main/MainActivity.kt
@@ -1,35 +1,126 @@
 package com.shino72.physical.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.games.Games
+import com.google.android.gms.games.LeaderboardsClient
+import com.google.firebase.auth.FirebaseAuth
 import com.shino72.physical.R
 import com.shino72.physical.databinding.ActivityMainBinding
+import com.shino72.physical.ui.global.MyApplication
+
 
 class MainActivity : AppCompatActivity() {
     private val vm : MainViewModel by viewModels()
+    private lateinit var auth: FirebaseAuth
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var leaderboardsClient: LeaderboardsClient
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.mainViewModel = vm
         setContentView(binding.root)
-
+        binding.rankingShowBtn.setOnClickListener {
+            showLeaderboard()
+        }
+        vm.score.observe(this, Observer {
+            if(it != -1.0)
+            {
+                submitScoreToLeaderboard(it.toLong() * 10)
+            }
+        })
         vm.status.observe(this, Observer {
             when(it)
             {
                 Status.FAST -> {
-                    binding.gameBtn.setBackgroundColor(resources.getColor(R.color.pauseColor))
+                    binding.ranking.visibility = View.GONE
+                    binding.gameBtn.setBackgroundColor(resources.getColor(com.shino72.physical.R.color.pauseColor))
                 }
                 Status.START -> {
-                    binding.gameBtn.setBackgroundColor(resources.getColor(R.color.startColor))
+                    binding.ranking.visibility = View.GONE
+                    binding.gameBtn.setBackgroundColor(resources.getColor(com.shino72.physical.R.color.startColor))
                 }
                 Status.MAIN -> {
-                    binding.gameBtn.setBackgroundColor(resources.getColor(R.color.mainColor))
+                    binding.ranking.visibility = View.VISIBLE
+                    binding.gameBtn.setBackgroundColor(resources.getColor(com.shino72.physical.R.color.mainColor))
                 }
                 else -> {
+                    binding.ranking.visibility = View.GONE
                 }
             }
         })
+
+    }
+    private fun googleSignIn() {
+        val signInIntent = GoogleSignIn.getClient(
+            this,
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).build()
+        ).signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    @SuppressLint("VisibleForTests")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                // 리더보드 사용을 위해 Google Play Games 서비스에 로그인
+                leaderboardsClient = Games.getLeaderboardsClient(this, account!!)
+                // 리더보드 표시
+            } catch (e: ApiException) {
+            }
+        }
+
+
+    }
+    private fun showLeaderboard() {
+        if(::leaderboardsClient.isInitialized)
+        {
+            leaderboardsClient.getLeaderboardIntent(MyApplication.LEADER_BOARD_ID)
+                .addOnSuccessListener { intent ->
+                    startActivityForResult(intent, RC_LEADERBOARD)
+                }
+                .addOnFailureListener { e ->
+                }
+        }
+        else {
+            googleSignIn()
+        }
+    }
+
+
+    private fun submitScoreToLeaderboard(score: Long) {
+        if(::leaderboardsClient.isInitialized)
+        {
+            leaderboardsClient.submitScoreImmediate(MyApplication.LEADER_BOARD_ID,score).addOnCompleteListener {
+                Toast.makeText(applicationContext, "성공", Toast.LENGTH_SHORT).show()
+            }
+                .addOnFailureListener {
+                    Toast.makeText(applicationContext, "실패", Toast.LENGTH_SHORT).show()
+
+                }
+        }
+        else {
+            googleSignIn()
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+        private const val RC_LEADERBOARD = 9002
     }
 }
--- app/src/main/java/com/shino72/physical/ui/main/MainViewModel.kt
+++ app/src/main/java/com/shino72/physical/ui/main/MainViewModel.kt
@@ -1,17 +1,23 @@
 package com.shino72.physical.ui.main
 
 import android.os.Looper
+import android.support.v4.os.IResultReceiver._Parcel
 import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.util.logging.Handler
+import com.google.android.gms.games.Games
+import com.google.android.gms.games.leaderboard.Leaderboards
 
 
 class MainViewModel : ViewModel() {
 
     private val _status = MutableLiveData<Status>()
     val status :  LiveData<Status> get() = _status
+
+
+    private val _score = MutableLiveData<Double>()
+    val score : LiveData<Double> get() = _score
 
     var msg = ObservableField<String>()
 
@@ -21,6 +27,7 @@ class MainViewModel : ViewModel() {
     private val handler = android.os.Handler(Looper.getMainLooper())
 
     init {
+        _score.value = -1.0
         _status.value = Status.MAIN
         msg.set("화면을 클릭하면 게임이 시작합니다.\n 녹색이 보이면 빠르게 누르세요.")
     }
@@ -32,8 +39,12 @@ class MainViewModel : ViewModel() {
             }
             Status.START -> {
                 endTime = System.nanoTime()
-                msg.set("당신의 반응속도는 ${(((endTime - startTime)/1_000_000_000.0).toDouble())}초 입니다.")
+                _score.value = ((endTime - startTime)/1_000_000.0).toDouble()
+                msg.set("당신의 반응속도는 \n ${score.value} ms 입니다.")
                 _status.value = Status.END
+
+
+
             }
             Status.PAUSE -> {
                 _status.value = Status.FAST
@@ -51,6 +62,8 @@ class MainViewModel : ViewModel() {
             }
         }
     }
+
+
 
     private fun gameStart()
     {
